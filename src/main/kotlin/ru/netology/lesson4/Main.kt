6b23a8c4b10c0ce2472976c8d0758d345065bab0
VM\\socrovisciuc <alexei-s1@yandex.ru>
2021-01-25 03:34:43 +0300

    Initial commit

---
+++ src/main/kotlin/ru/netology/lesson4/Main.kt
@@ -0,0 +1,71 @@
+package ru.netology.lesson4
+
+/**
+ * Домашнее задание к занятию «2.1. Функции, автотесты и Continuous Integration»
+ * Задача №1 - Максимальное покрытие
+ */
+
+enum class CardTypes {
+    Mastercard, Maestro, Visa, Mir, VKPay
+}
+
+fun main()
+{
+
+}
+
+/**
+ *
+ * Description
+ * calcPaymentCommission вычисляет комиссию в копейках
+ * cardType       - Тип карты/счёта (по умолчанию - Vk Pay)
+ * lastPayments   - Сумма предыдущих переводов в этом месяце (по умолчанию - 0)
+ * currentPayment - Сумма совершаемого перевода в копейках
+ * результат      - комиссия в копейках
+ */
+fun calcPaymentCommission (cardType : CardTypes = CardTypes.VKPay,
+                           lastMonthPayments : Long = 0, currentPayment : Long) : Long
+{
+    val noCommission = 0L
+    return when(cardType) {
+        CardTypes.Mastercard, CardTypes.Maestro ->
+            calcMastercardMaestroCommission(currentPayment, lastMonthPayments)
+        CardTypes.Visa, CardTypes.Mir -> calcVisaMirCommission          (currentPayment)
+        else                          -> noCommission
+    }
+}
+
+/**
+ * Description
+ * calcMastercardMaestroCommission
+ * вычисляет комиссию в копейках для Mastercard, Maestro
+ * currentPayment - Сумма совершаемого перевода в копейках
+ * результат      - комиссия в копейках
+ */
+fun calcMastercardMaestroCommission(currentPayment : Long, lastMonthPayments : Long) : Long
+{
+    val noCommission                = 0L
+    val commissionPercent           = 0.6F
+    val commissionFix               = 20_00.0F
+    val mastercardMaestroMonthLimit = 75_000_00
+    return if (lastMonthPayments < mastercardMaestroMonthLimit) {
+        noCommission
+    } else {
+        (currentPayment * commissionPercent / 100.0F + commissionFix).toLong()
+    }
+}
+
+/**
+ * Description
+ * calcVisaMirCommission
+ * вычисляет комиссию в копейках для Visa, Mir
+ * currentPayment - Сумма совершаемого перевода в копейках
+ * результат      - комиссия в копейках
+ */
+fun calcVisaMirCommission(currentPayment : Long) : Long
+{
+    val commissionPercent = 0.75F
+    val commissionMin     = 35_00L
+    val result = (currentPayment * commissionPercent / 100.0F).toLong()
+    return if (result >= commissionMin) result else commissionMin
+}
---
+++ src/test/kotlin/ru/netology/lesson4/MainKtTest.kt
@@ -0,0 +1,114 @@
+package ru.netology.lesson4
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+    //Минимальная коммиссия Виза/Мир
+    @Test
+    fun calcVisaMirCommission_commissionMin() {
+        val currentPayment = 10_00L
+        val result = calcVisaMirCommission(currentPayment)
+        assertEquals(35_00L, result)
+    }
+    //стандартная коммиссия Виза/Мир
+    @Test
+    fun calcVisaMirCommission_commissionPercent() {
+        val currentPayment = ((35_00L + 1L) * 100L / 0.75F).toLong()
+        val result = calcVisaMirCommission(currentPayment)
+        assertEquals((currentPayment * 0.75F / 100.0F).toLong(), result)
+    }
+    //месячный лимит не достигнут
+    @Test
+    fun calcMastercardMaestro_noCommission() {
+        val currentPayment = 10_00L
+        val monthLimit     = 10_000_00L
+        val result = calcMastercardMaestroCommission(currentPayment, monthLimit)
+        assertEquals(0L, result)
+    }
+    //месячный лимит превышен
+    @Test
+    fun calcMastercardMaestro_CommissionPercent() {
+        val currentPayment = 1_000_00L  // 1000 * 0.6% = 6р + 20р фикс = 26р
+        val monthLimit     = 80_000_00L //>75_000_00
+        val result = calcMastercardMaestroCommission(currentPayment, monthLimit)
+        assertEquals(26_00L, result)
+    }
+    //тест маестро
+    @Test
+    fun calcPaymentCommission_Maestro_noCommission() {
+        val currentPayment = 10_00L
+        val monthLimit     = 10_000_00L
+        val cardType       = CardTypes.Maestro
+        val result = calcPaymentCommission(
+            cardType          = cardType,
+            lastMonthPayments = monthLimit,
+            currentPayment    = currentPayment
+        )
+        assertEquals(0L, result)
+    }
+    //тест мастеркард
+    @Test
+    fun calcPaymentCommission_Mastercard_noCommission() {
+        val currentPayment = 10_00L
+        val monthLimit     = 10_000_00L
+        val cardType       = CardTypes.Mastercard
+        val result = calcPaymentCommission(
+            cardType          = cardType,
+            lastMonthPayments = monthLimit,
+            currentPayment    = currentPayment
+        )
+        assertEquals(0L, result)
+    }
+
+    //Минимальная коммиссия Виза
+    @Test
+    fun calcPaymentCommission_Visa_minCommission() {
+        val currentPayment = 10_00L
+        val monthLimit     = 10_000_00L
+        val cardType       = CardTypes.Visa
+        val result = calcPaymentCommission(
+            cardType          = cardType,
+            lastMonthPayments = monthLimit,
+            currentPayment    = currentPayment
+        )
+        assertEquals(35_00L, result)
+    }
+
+    //Минимальная коммиссия Мир
+    @Test
+    fun calcPaymentCommission_Mir_minCommission() {
+        val currentPayment = 10_00L
+        val monthLimit     = 10_000_00L
+        val cardType       = CardTypes.Mir
+        val result = calcPaymentCommission(
+            cardType          = cardType,
+            lastMonthPayments = monthLimit,
+            currentPayment    = currentPayment
+        )
+        assertEquals(35_00L, result)
+    }
+
+    //VKPay
+    @Test
+    fun calcPaymentCommission_VkPay_noCommission() {
+        val currentPayment = 10_00L
+        val monthLimit     = 10_000_00L
+        val cardType       = CardTypes.VKPay
+        val result = calcPaymentCommission(
+            cardType          = cardType,
+            lastMonthPayments = monthLimit,
+            currentPayment    = currentPayment
+        )
+        assertEquals(0L, result)
+    }
+
+    //параметры по умолчанию
+    @Test
+    fun calcPaymentCommission_defaultParams() {
+        val currentPayment = 10_00L
+        val result = calcPaymentCommission(currentPayment = currentPayment)
+        assertEquals(0L, result)
+    }
+}
